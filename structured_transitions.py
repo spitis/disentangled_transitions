999a8340874b6e71a9b580af0516093e53d57da0
spitis <silviu.pitis@gmail.com>
2020-03-03 12:08:40 -0500

    static laplacian optimization works but dynamics doesnt

--- structured_transitions.py
+++ structured_transitions.py
@@ -3,9 +3,11 @@ import torch
 import torch.nn.functional as F
 import torch.nn as nn
 
+
 class GELU(nn.Module):
   def forward(self, input):
     return F.gelu(input)
+
 
 class Randfn(nn.Module):
   """
@@ -14,25 +16,21 @@ class Randfn(nn.Module):
   """
   def __init__(self, num_in, num_out):
     super().__init__()
-    self.f = nn.Sequential(*[
-      nn.Linear(num_in, 32),
-      GELU(),
-      nn.Linear(32, num_out)
-    ])
-  
+    self.f = nn.Sequential(*[nn.Linear(num_in, 32), GELU(), nn.Linear(32, num_out)])
+
   def forward(self, x):
     return self.f(x)
 
+
 class MaskedLinear(nn.Linear):
   """ same as Linear except returns absolute weights """
-  
   @property
   def mask(self):
     return torch.abs(self.weight).T
-  
+
+
 class MaskedNetwork(nn.Module):
   """ same as MLP except has a mask on the weights """
-
   def __init__(self, in_features, out_features, num_hidden_layers=2, num_hidden_units=256):
     super().__init__()
     layers = [MaskedLinear(in_features, num_hidden_units), nn.ReLU()]
@@ -45,45 +43,45 @@ class MaskedNetwork(nn.Module):
   @property
   def mask(self):
     res = self.f[0].mask
-    
+
     for layer in self.f[1:]:
       if hasattr(layer, 'mask'):
         res = res.matmul(layer.mask)
-    
+
     return res
-    
+
   def forward(self, x):
     return self.f(x)
 
+
 class MixtureOfMaskedNetworks(nn.Module):
-  
   def __init__(self, in_features, out_features, num_components=10, num_hidden_layers=2, num_hidden_units=256):
     super().__init__()
-    self.components = nn.ModuleList([MaskedNetwork(
-      in_features, out_features, num_hidden_layers, num_hidden_units) for _ in range(num_components)])
-    
-    self.attn_net = nn.Sequential(*[
-      nn.Linear(in_features, num_hidden_units), 
-      nn.ReLU(inplace=True),
-      nn.Linear(num_hidden_units, num_components)
-    ])
-    
+    self.components = nn.ModuleList(
+        [MaskedNetwork(in_features, out_features, num_hidden_layers, num_hidden_units) for _ in range(num_components)])
+
+    self.attn_net = nn.Sequential(
+        *[nn.Linear(in_features, num_hidden_units),
+          nn.ReLU(inplace=True),
+          nn.Linear(num_hidden_units, num_components)])
+
   def dynamic_mask(self, x):
     return self.forward_with_mask(x)[1]
-        
+
   def forward(self, x):
     return self.forward_with_mask(x)[0]
-  
+
   def forward_with_mask(self, x):
-    masks = torch.stack([c.mask for c in self.components])[None] # 1 x C x I x O
-    comps = torch.stack([c(x) for c in self.components]).transpose(0, 1) # B x C x O
-    
-    attns = F.softmax(self.attn_net(x), -1) # B x C
-    
-    mask = (masks * attns[:,:,None,None]).sum(dim=1) # B x I x O
-    res = (comps * attns[:, :, None]).sum(dim=1) # B x O
+    masks = torch.stack([c.mask for c in self.components])[None]  # 1 x C x I x O
+    comps = torch.stack([c(x) for c in self.components]).transpose(0, 1)  # B x C x O
+
+    attns = F.softmax(self.attn_net(x), -1)  # B x C
+
+    mask = (masks * attns[:, :, None, None]).sum(dim=1)  # B x I x O
+    res = (comps * attns[:, :, None]).sum(dim=1)  # B x O
 
     return res, mask, attns
+
 
 def gen_samples_static(num_seqs=16, seq_len=12, splits=[5, 3, 2]):
   """
@@ -102,8 +100,8 @@ def gen_samples_static(num_seqs=16, seq_len=12, splits=[5, 3, 2]):
     next_factors = [fn(f) for f, fn in zip(factors, fns)]
     seq.append(torch.cat(next_factors, 1))
 
+  return fns, (torch.cat(seq[:-1]), torch.cat(seq[1:]))
 
-  return fns, (torch.cat(seq[:-1]),torch.cat(seq[1:])) 
 
 def gen_samples_dynamic(num_seqs=16, seq_len=12, splits=[5, 3, 2], epsilon=2.):
   """
@@ -117,32 +115,33 @@ def gen_samples_dynamic(num_seqs=16, seq_len=12, splits=[5, 3, 2], epsilon=2.):
   fns = []
   global_fns = []
   total_global_interactions = 0
-  
+
   for split in splits:
     fns.append(Randfn(split, split))
     global_fns.append(Randfn(split, sum(splits)))
-  
+
   for i in range(seq_len):
     factors = seq[-1].split(splits, dim=1)
-    next_factors = [fn(f / f.norm(dim=-1).view(-1,1)) for f, fn in zip(factors, fns)]
+    next_factors = [fn(f / f.norm(dim=-1).view(-1, 1)) for f, fn in zip(factors, fns)]
     seq.append(torch.cat(next_factors, 1))
 
     for f, gfn in zip(factors, global_fns):
-      mask = (f.norm(dim=-1) > epsilon).to(torch.float32).view(-1,1)
+      mask = (f.norm(dim=-1) > epsilon).to(torch.float32).view(-1, 1)
       total_global_interactions += mask.sum()
-      seq[-1] = seq[-1] + gfn(f / f.norm(dim=-1).view(-1,1))*mask
-    
-  return total_global_interactions, fns, (torch.cat(seq[:-1]),torch.cat(seq[1:]))
+      seq[-1] = seq[-1] + gfn(f / f.norm(dim=-1).view(-1, 1)) * mask
+
+  return total_global_interactions, fns, (torch.cat(seq[:-1]), torch.cat(seq[1:]))
+
 
 class TransitionsData(torch.utils.data.Dataset):
   def __init__(self, samples):
     x, y = samples
     self.x = x.detach()
     self.y = y.detach()
-      
+
   def __len__(self):
     return len(self.x)
-  
+
   def __getitem__(self, idx):
     if torch.is_tensor(idx):
       idx = idx.tolist()
