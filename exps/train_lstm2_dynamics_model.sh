15bfacc94921cc93505f4df3dd6ee7e2c5d259f7
Elliot Creager <creager@cs.toronto.edu>
2020-04-06 14:27:02 -0400

    train deeper LSTM baeline

--- exps/train_lstm2_dynamics_model.sh
+++ exps/train_lstm2_dynamics_model.sh
@@ -1,2 +1,8 @@
-#!/bin/bash
-srun --p gpu --gres=gpu:1 --mem=24G -c 4 python model_based_rollouts.py --model_type lstm --num_sprites 6 --imagedim 20 --seed 0 --num_examples 10000 --patience_epochs 100 --max_episode_length 500 --results_dir /scratch/gobi1/creager/disentangled_transitions/model_based_rollouts/lstm2 --num_lstm_layers 2
+#!/bin/bash -x
+#SBATCH --gres=gpu:0
+#SBATCH -p cpu
+#SBATCH -c 4
+#SBATCH --mem=12G
+#SBATCH -o ./slurm_output/%J.out # this is where the output goes
+
+python model_based_rollouts.py --model_type lstm --num_sprites 6 --imagedim 20 --seed 0 --num_examples 10000 --patience_epochs 100 --max_episode_length 500 --results_dir /scratch/gobi1/creager/disentangled_transitions/model_based_rollouts/lstm2 --num_lstm_layers 2
